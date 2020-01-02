253b98b808d979380204dd45a1ac73e6c70c3018
nelsy <nelson.kim.1994@gmail.com>
2020-01-02 01:22:10 -0500

    button styling changes

--- client/src/layout/header.jsx
+++ client/src/layout/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header () {
 	return (
 		<div>
-			<h1 style={{ marginBottom: '3px' }}>Pro Bans</h1> 
+			<h1 style={{ marginBottom: '3px' }}>Mastery Check</h1> 
 			<div style={{ height: '1px', backgroundColor: 'white', marginBottom: '25px', marginTop: '2px' }}></div>
 		</div>
 	)
